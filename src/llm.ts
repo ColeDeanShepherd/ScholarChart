447161a35c0d2dce32673175bd37a950ff8e7dd9
Cole Shepherd <cole@coledeanshepherd.com>
2024-12-24 08:47:27 -0800

    Small fix.

--- src/llm.ts
+++ src/llm.ts
@@ -1,12 +1,12 @@
 import OpenAI from 'openai';
 import { getOpenAIKey as getOpenAIAPIKey } from './config';
 
-const client = new OpenAI({
-  apiKey: getOpenAIAPIKey(), // This is the default and can be omitted
-  dangerouslyAllowBrowser: true
-});
-
 export async function runOpenAITest() {
+  const client = new OpenAI({
+    apiKey: getOpenAIAPIKey(), // This is the default and can be omitted
+    dangerouslyAllowBrowser: true
+  });
+
   const chatCompletion = await client.chat.completions.create({
     messages: [{ role: 'user', content: 'Say this is a test' }],
     model: 'gpt-4o',
