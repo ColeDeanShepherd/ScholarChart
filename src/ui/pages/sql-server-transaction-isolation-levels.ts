b2495713c50de1f999912e1165f526bceebbf924
Cole Shepherd <cole@coledeanshepherd.com>
2024-12-29 18:23:31 -0800

    Progress

--- src/ui/pages/sql-server-transaction-isolation-levels.ts
+++ src/ui/pages/sql-server-transaction-isolation-levels.ts
@@ -1,6 +1,8 @@
 import logo from '../../logo.svg';
 import { getCurQueryParams } from "../../web-lib";
 import { IRoute } from '../../router';
+import { isDevEnv } from '../../config';
+import { article, button, h2, h3, section, text } from '../../framework/ui/ui-core';
 
 interface ITransactionIsolationLevelInfo {
   nameHtml: string;
@@ -34,103 +36,104 @@ function infoToTr(info: ITransactionIsolationLevelInfo): string {
     </tr>`);
 }
 
+
+const transactionIsolationLevels: ITransactionIsolationLevelInfo[] = [
+  {
+    nameHtml: 'READ UNCOMMITTED',
+    preventsDirtyReads: false,
+    preventsNonRepeatableReads: false,
+    preventsPhantomReads: false,
+    throughputRating: 4,
+    additionalIoAndTempDbUsageRating: 0,
+    useCasesRemarksHtml: `
+      <ul>
+        <li><u>Example Use</u>: Generating approximate reports in real-time dashboards.</li>
+      </ul>
+    `
+  },
+  {
+    nameHtml: 'READ COMMITTED<br /><small><span class="no-word-break">(w/READ_COMMITTED_SNAPSHOT on)</span></small>',
+    preventsDirtyReads: true,
+    preventsNonRepeatableReads: false,
+    preventsPhantomReads: false,
+    throughputRating: 4,
+    additionalIoAndTempDbUsageRating: 1,
+    useCasesRemarksHtml: `
+      <ul>
+        <li><u>Example Use</u>: Typical OLTP workloads.</li>
+        <li>The default isolation level in Azure SQL.</li>
+        <li>Uses row versioning to prevent dirty reads without blocking.</li>
+      </ul>
+    `
+  },
+  {
+    nameHtml: 'READ COMMITTED<br /><small><span class="no-word-break">(w/READ_COMMITTED_SNAPSHOT off)</span></small>',
+    preventsDirtyReads: true,
+    preventsNonRepeatableReads: false,
+    preventsPhantomReads: false,
+    throughputRating: 3,
+    additionalIoAndTempDbUsageRating: 0,
+    useCasesRemarksHtml: `
+      <ul>
+        <li><u>Example Use</u>: OLTP workloads where increased I/O &amp; TempDB usage is a problem.</li>
+        <li>The default isolation level in SQL Server.</li>
+        <li>Uses locks to prevent dirty reads.</li>
+      </ul>
+    `
+  },
+  {
+    nameHtml: 'REPEATABLE READ',
+    preventsDirtyReads: true,
+    preventsNonRepeatableReads: true,
+    preventsPhantomReads: false,
+    throughputRating: 2,
+    additionalIoAndTempDbUsageRating: 0,
+    useCasesRemarksHtml: `
+      <ul>
+        <li><u>Example Use</u>: Financial applications calculating intermediate results based on multiple reads.</li>
+      </ul>
+    `
+  },
+  {
+    nameHtml: 'SNAPSHOT',
+    preventsDirtyReads: true,
+    preventsNonRepeatableReads: true,
+    preventsPhantomReads: true,
+    throughputRating: 4,
+    additionalIoAndTempDbUsageRating: 2,
+    useCasesRemarksHtml: `
+      <ul>
+        <li>
+          <u>Example Uses</u>:
+          <ul>
+            <li>Reporting and analytics workloads requiring a consistent snapshot of data.</li>
+            <li>Ensuring application sees stable/coherent snapshots when querying change-tracked data.</li>
+          </ul>
+        </li>
+        <li>Uses versioning to provide a consistent view of data from start of transaction without blocking.</li>
+      </ul>
+    `
+  },
+  {
+    nameHtml: 'SERIALIZABLE',
+    preventsDirtyReads: true,
+    preventsNonRepeatableReads: true,
+    preventsPhantomReads: true,
+    throughputRating: 1,
+    additionalIoAndTempDbUsageRating: 0,
+    useCasesRemarksHtml: `
+      <ul>
+        <li><u>Example Use</u>: Financial applications where transactions involve critical integrity constraints.</li>
+        <li>Behaves like only one transaction can access data at a time.</li>
+      </ul>
+    `
+  }
+];
+
 export const sqlServerTransactionIsolationLevelsRoute: IRoute = {
   pathname: '/sql-server-transaction-isolation-levels',
   title: 'SQL Server Transaction Isolation Levels',
   renderFn: routeContainerElem => {
-    const transactionIsolationLevels: ITransactionIsolationLevelInfo[] = [
-      {
-        nameHtml: 'READ UNCOMMITTED',
-        preventsDirtyReads: false,
-        preventsNonRepeatableReads: false,
-        preventsPhantomReads: false,
-        throughputRating: 4,
-        additionalIoAndTempDbUsageRating: 0,
-        useCasesRemarksHtml: `
-          <ul>
-            <li><u>Example Use</u>: Generating approximate reports in real-time dashboards.</li>
-          </ul>
-        `
-      },
-      {
-        nameHtml: 'READ COMMITTED<br /><small><span class="no-word-break">(w/READ_COMMITTED_SNAPSHOT on)</span></small>',
-        preventsDirtyReads: true,
-        preventsNonRepeatableReads: false,
-        preventsPhantomReads: false,
-        throughputRating: 4,
-        additionalIoAndTempDbUsageRating: 1,
-        useCasesRemarksHtml: `
-          <ul>
-            <li><u>Example Use</u>: Typical OLTP workloads.</li>
-            <li>The default isolation level in Azure SQL.</li>
-            <li>Uses row versioning to prevent dirty reads without blocking.</li>
-          </ul>
-        `
-      },
-      {
-        nameHtml: 'READ COMMITTED<br /><small><span class="no-word-break">(w/READ_COMMITTED_SNAPSHOT off)</span></small>',
-        preventsDirtyReads: true,
-        preventsNonRepeatableReads: false,
-        preventsPhantomReads: false,
-        throughputRating: 3,
-        additionalIoAndTempDbUsageRating: 0,
-        useCasesRemarksHtml: `
-          <ul>
-            <li><u>Example Use</u>: OLTP workloads where increased I/O &amp; TempDB usage is a problem.</li>
-            <li>The default isolation level in SQL Server.</li>
-            <li>Uses locks to prevent dirty reads.</li>
-          </ul>
-        `
-      },
-      {
-        nameHtml: 'REPEATABLE READ',
-        preventsDirtyReads: true,
-        preventsNonRepeatableReads: true,
-        preventsPhantomReads: false,
-        throughputRating: 2,
-        additionalIoAndTempDbUsageRating: 0,
-        useCasesRemarksHtml: `
-          <ul>
-            <li><u>Example Use</u>: Financial applications calculating intermediate results based on multiple reads.</li>
-          </ul>
-        `
-      },
-      {
-        nameHtml: 'SNAPSHOT',
-        preventsDirtyReads: true,
-        preventsNonRepeatableReads: true,
-        preventsPhantomReads: true,
-        throughputRating: 4,
-        additionalIoAndTempDbUsageRating: 2,
-        useCasesRemarksHtml: `
-          <ul>
-            <li>
-              <u>Example Uses</u>:
-              <ul>
-                <li>Reporting and analytics workloads requiring a consistent snapshot of data.</li>
-                <li>Ensuring application sees stable/coherent snapshots when querying change-tracked data.</li>
-              </ul>
-            </li>
-            <li>Uses versioning to provide a consistent view of data from start of transaction without blocking.</li>
-          </ul>
-        `
-      },
-      {
-        nameHtml: 'SERIALIZABLE',
-        preventsDirtyReads: true,
-        preventsNonRepeatableReads: true,
-        preventsPhantomReads: true,
-        throughputRating: 1,
-        additionalIoAndTempDbUsageRating: 0,
-        useCasesRemarksHtml: `
-          <ul>
-            <li><u>Example Use</u>: Financial applications where transactions involve critical integrity constraints.</li>
-            <li>Behaves like only one transaction can access data at a time.</li>
-          </ul>
-        `
-      }
-    ];
-
     const tableHtml = `
       <table class="striped">
         <thead>
@@ -166,8 +169,8 @@ export const sqlServerTransactionIsolationLevelsRoute: IRoute = {
         ${tableHtml}
 
         <div style="display: flex; justify-content: space-between; align-items: center;">
-          <div class="logo-with-name"><img src="${logo}" alt="ScholarChart" /> ScholarChart.com</div>
-          <div>v3 (Dec. 27, 2024)</div>
+          <div class="logo-with-name"><div class="show-only-in-screenshot"><img src="${logo}" alt="ScholarChart" /> ScholarChart.com</div></div>
+          <div>v4 (Dec. 29, 2024)</div>
         </div>
         
         <p class="sources hide-in-screenshot" style="margin-top: 2rem;">Source: <a href="https://learn.microsoft.com/en-us/sql/relational-databases/sql-server-transaction-locking-and-row-versioning-guide" target="_blank">https://learn.microsoft.com/en-us/sql/relational-databases/sql-server-transaction-locking-and-row-versioning-guide</a></p>
@@ -186,7 +189,20 @@ export const sqlServerTransactionIsolationLevelsRoute: IRoute = {
       routeContainerElem.classList.remove('container');
       routeContainerElem.classList.remove('no-padding-top');
       routeContainerElem.classList.add('container-fluid');
-      document.getElementsByClassName('hide-in-screenshot')[0].classList.add('hide');
+    }
+
+    if (isDevEnv()) {
+      const container = document.getElementsByClassName('sql-server-transaction-isolation-levels')[0];
+      
+      const quizSection = section({ class: 'hide-in-screenshot' }, [
+        h2([ text('Test your knowledge:') ]),
+        article([
+          h3([ text('Does "READ UNCOMMITTED" prevent dirty reads?') ]),
+          button({ onClick: () => alert('Incorrect') }, [ text('Yes') ]),
+          button({ onClick: () => alert('Correct') }, [ text('No') ])
+        ])
+      ]);
+      container.appendChild(quizSection);
     }
   }
 };
